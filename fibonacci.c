9d2b89b3d9caad39bdc1e3c4163a391efca8ca5e
ronaldoyang <ronaldo.yang@yahoo.com.br>
2016-03-23 06:58:02 -0300

    Update fibonacci.c
    arrumando espaçamentos

--- fibonacci.c
+++ fibonacci.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int fibonacci(int n){
-  if (n == 1 || n == 2)
-    return 1;
-  return fibonacci(n-1) + fibonacci(n-2);
+	if (n == 1 || n == 2)
+		return 1;
+	return fibonacci(n-1) + fibonacci(n-2);
 }
 
 void printFibonacci(int n) {
