fb17ea5e37d7a5b55f6586bf47f0cf74cbcb4dfa
ronaldoyang <ronaldo.yang@yahoo.com.br>
2016-03-23 06:58:58 -0300

    Update integral.c
    ajuste espaçamento

--- integral.c
+++ integral.c
@@ -7,11 +7,11 @@
 #define seed time(NULL)
 
 void integral(int s){
-  int i, t = 0;
-  srand(seed);
-  for (i = 0; i < s; i++)
-    if(1.0 / cos((double)rand() / RAND_MAX) >= (double)rand() * sec1 / RAND_MAX)
-      t++;
+	int i, t = 0;
+	srand(seed);
+	for (i = 0; i < s; i++)
+		if(1.0 / cos((double)rand() / RAND_MAX) >= (double)rand() * sec1 / RAND_MAX)
+			t++;
 	printf("P4: Número de pontos t = %d\n", t);
 	printf("P4: Número de pontos s = %d\n", s);
 	printf("P4: Área estimada = %f\n", (double)t * sec1 / s);
